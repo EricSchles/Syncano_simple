1d4e94c322db2f8305f8ae1908e3e504ed51e30f
Eric Schles <ericschles@gmail.com>
2015-05-21 13:31:20 -0700

    all the things

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 import syncano
 import pickle
 from flask import Flask,render_template,url_for, request
+import json
 import random
 
 class Voting:
@@ -29,13 +30,12 @@ class RandomData:
     def create_schema(self):
         self.instance.classes.create(name="random_data",schema='[{"name":"data","type":"integer"}]')
     def create_data_set(self):
-        for i in xrange(100): 
-            data = random.randint(10,100000)
+        for i in xrange(10): 
+            data = random.randint(10,100)
             obj = self.random_data.objects.create(data=data)
             obj.save()
     def get_all_data(self):
         return [elem.data for elem in list(self.random_data.objects.all())]
-
 
 app = Flask(__name__)
 
@@ -50,8 +50,14 @@ def index():
     return render_template("index.html",votes=vote_obj.get_all_votes())
 
 
+@app.route("/viz",methods=["GET","POST"])
+def viz():
+    rando = RandomData()
+    rando.create_data_set()
+    data = rando.get_all_data()
+    return render_template("viz.html",data=json.dumps(data))
+
+
 if __name__ == '__main__':
-    rand = RandomData()
-    #rand.create_data_set()
-    print rand.get_all_data()
-#    app.run(debug=True)
+    
+    app.run(debug=True)
